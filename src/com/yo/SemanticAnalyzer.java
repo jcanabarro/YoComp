96c3701eb66a45356df7dbcc94d3294d4b468975
jcanabarro <joaovictorcana@gmail.com>
2018-11-14 11:38:43 -0200

    Stable version of TAC, just need to find all the usages

--- src/com/yo/SemanticAnalyzer.java
+++ src/com/yo/SemanticAnalyzer.java
@@ -1,6 +1,11 @@
 package com.yo;
 
 import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 // TODO: Need apply 3 end code to expressions
 // TODO: Need apply type verification
@@ -8,17 +13,24 @@ import java.util.Stack;
 
 class SemanticAnalyzer {
 
-    private int label_counter;
     private String code;
+    private Integer label_counter;
+    private Integer temp_counter;
+
+    SemanticAnalyzer(){
+        code = "";
+        label_counter = 0;
+        temp_counter = 0;
+    }
 
     Token codeGenerator(String s, String prod, Stack<Token> pilha) {
         Token t_prod = new Token("nao_terminal", prod);
 
         Token token;
-        System.out.println(s + " " + prod);
+//        System.out.println(s + " " + prod);
         switch(Integer.valueOf(s)) {
             case 1:
-//                System.out.println("Codigo intermediario gerado com sucesso");
+                System.out.println("Codigo intermediario gerado com sucesso");
                 break;
             case 2:
                 Token token_type;
@@ -31,7 +43,7 @@ class SemanticAnalyzer {
             case 5:
             case 6:
                 token = pilha.pop();
-                t_prod.setCodigo("\t" + token.getCodigo());
+                t_prod.setCodigo(token.getCodigo());
                 break;
             case 7: // attributions
             case 8: // attributions
@@ -39,7 +51,17 @@ class SemanticAnalyzer {
                 token_attr = pilha.pop();
                 pilha.pop();
                 token = pilha.pop();
-                t_prod.setCodigo(token_attr.getValor() + " = " + token.getCodigo());
+                code = token_attr.getValor() + " = " + token.getCodigo();
+                List<String> assembly_expression = Arrays.asList(code.split(" "));
+                if (assembly_expression.size() > 3) {
+                    ArrayList<String> expression = new ArrayList<>(assembly_expression);
+                    expression.remove(0);
+                    expression.remove(0);
+                    String tac = "";
+                    List<String> formattedTac = format3End(expression, tac, token_attr.getValor());
+                    code = String.join("\n ", formattedTac);
+                }
+                t_prod.setCodigo(code);
                 break;
             case 9: // Reserved word yoint
             case 10: // Reserved word yofloat
@@ -97,13 +119,10 @@ class SemanticAnalyzer {
                 break;
             case 35: // SWITCH
             case 36: // SWITCH
-//                formatSwitch(pilha, t_prod);
-//                break;
             case 37: // CASE
             case 38: // CASE
             case 39: // CASE
             case 40: // CASE
-//                formatCase(pilha, t_prod);
                 break;
             case 41:
                 token = pilha.pop();
@@ -173,15 +192,34 @@ class SemanticAnalyzer {
             case 69:
             case 70:
                 token = pilha.pop();
-                t_prod.setCodigo(token.getValor() + pilha.pop().getValor());
+                code += this.getTempCounter()  + " = " + token.getValor() + " + 1 \n";
+                t_prod.setCodigo(token.getValor() + " = " + this.getSpecialTempCounter());
                 break;
             case 71: break;
             case 72: break;
         }
-        // TODO remover isso depois
+//        // TODO remover isso depois
 //        if(!t_prod.getCodigo().equals(""))
 //            System.out.println(t_prod.getCodigo());
         return t_prod;
+    }
+
+    private List<String> format3End(List<String> assembly_expression, String expr, String attribution) {
+        if (assembly_expression.size() > 2) {
+            String temp = this.getTempCounter();
+            expr +=  temp + " = ";
+            expr += assembly_expression.get(0) + " ";
+            assembly_expression.remove(0);
+            expr += assembly_expression.get(0) + " ";
+            assembly_expression.remove(0);
+            expr += assembly_expression.get(0) + " ";
+            assembly_expression.remove(0);
+            assembly_expression.add(0, temp);
+            expr += "\n";
+            return format3End(assembly_expression, expr, attribution);
+        }
+        expr += attribution + " = " + this.getSpecialTempCounter();
+        return Collections.singletonList(expr);
     }
 
     private void formatExpression(Stack<Token> pilha, Token t_prod) {
@@ -206,13 +244,6 @@ class SemanticAnalyzer {
         code += "if " + token_statement.getCodigo() + " = false goto "+ t_prod.getFim() + "\n";
         code += token_expr.getCodigo() + "\n"+ "goto "+ t_prod.getInicio() + "\n" + t_prod.getFim()+" : ";
         t_prod.setCodigo(code);
-    }
-
-    // TODO remove this, because this it's only for debug, just only for the final version
-    private void printAttr(Token token_attr) {
-        System.out.println(token_attr.getCodigo());
-        System.out.println(token_attr.getAtributo());
-        System.out.println(token_attr.getValor());
     }
 
     private void formatOperation(Stack<Token> pilha, Token t_prod, String type) {
@@ -346,5 +377,14 @@ class SemanticAnalyzer {
     private String getLabelCounter(){
         label_counter++;
         return "Label " + this.label_counter;
+    }
+
+    private String getTempCounter(){
+        temp_counter++;
+        return "T"+ this.temp_counter;
+    }
+
+    private String getSpecialTempCounter(){
+        return "T"+ this.temp_counter;
     }
 }
